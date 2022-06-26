56fa71c746f626481aa535ce2603190af0af0387
VivianMPQ <104032517+VivianMPQ@users.noreply.github.com>
2022-06-25 19:55:40 -0500

    feat: added frontend

--- Frontend/CorvusEconomy/karma.conf.js
+++ Frontend/CorvusEconomy/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/corvus-economy'),
+      dir: require('path').join(__dirname, './coverage/frontend-corvus-economy'),
       subdir: '.',
       reporters: [
         { type: 'html' },
